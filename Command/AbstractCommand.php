156390cf81665c0f8fda15af6211c4527c88ea17
Arnaud Frézet <arnaudfrezet@gmail.com>
2017-10-04 23:35:29 +0200

    make the event dispatcher available as class attribute in the AbstractCommand class

--- Command/AbstractCommand.php
+++ Command/AbstractCommand.php
@@ -13,6 +13,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Yaml\Yaml;
 
 abstract class AbstractCommand extends Command implements ContainerAwareInterface
@@ -31,6 +32,11 @@ abstract class AbstractCommand extends Command implements ContainerAwareInterfac
      * @var ContainerInterface
      */
     protected $container;
+    
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $eventDispatcher;
 
     /**
      * Build tasks from the configuration array.
@@ -62,7 +68,7 @@ abstract class AbstractCommand extends Command implements ContainerAwareInterfac
     protected function buildFilters(array $configuration)
     {
         $this->io->text('- Building filters...');
-        $builder = new FilterBuilder($this->container->get('event_dispatcher'));
+        $builder = new FilterBuilder($this->eventDispatcher);
 
         $filters = $builder->build($configuration);
 
@@ -94,12 +100,15 @@ abstract class AbstractCommand extends Command implements ContainerAwareInterfac
 
         return $configuration['jk_assets']['tasks'];
     }
-
+    
     /**
      * Load the configuration from a yml file or the container, according to the given option.
      *
      * @param InputInterface $input
+     *
      * @return array
+     *
+     * @throws Exception
      */
     protected function loadConfiguration(InputInterface $input)
     {
@@ -108,6 +117,10 @@ abstract class AbstractCommand extends Command implements ContainerAwareInterfac
         if ($input->hasOption('config') && $file = $input->getOption('config')) {
             $configuration = $loader->loadFromFile($file);
         } else {
+            
+            if (null === $this->container) {
+                throw new Exception('Unable to find the assets configuration from the container');
+            }
             $configuration = $loader->loadFromContainer($this->container);
         }
 
@@ -122,5 +135,6 @@ abstract class AbstractCommand extends Command implements ContainerAwareInterfac
     public function setContainer(ContainerInterface $container = null)
     {
         $this->container = $container;
+        $this->eventDispatcher = $this->container->get('event_dispatcher');
     }
 }
